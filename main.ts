72f18e435b0764216cb9441f0960a6389a646a22
aws <149275321+issandreadadesigner@users.noreply.github.com>
2026-09-24 12:37:09 -0400

    Changed programming language to python

--- main.ts
+++ main.ts
@@ -1,22 +1,15 @@
-input.onGesture(Gesture.LogoUp, function () {
+input.onGesture(Gesture.LogoUp, function on_gesture_logo_up() {
+    
     Index = randint(0, text_list.length - 1)
-    basic.showString("" + (text_list[Index]))
+    basic.showString("" + text_list[Index])
 })
-input.onGesture(Gesture.ScreenUp, function () {
+input.onGesture(Gesture.ScreenUp, function on_gesture_screen_up() {
     game.addScore(1)
 })
-input.onGesture(Gesture.ScreenDown, function () {
+input.onGesture(Gesture.ScreenDown, function on_gesture_screen_down() {
     game.removeLife(1)
 })
 let Index = 0
-let text_list: string[] = []
+let text_list : string[] = []
 game.startCountdown(30000)
-text_list = [
-"ant",
-"bot",
-"cat",
-"dog",
-"egg",
-"fan",
-"goat"
-]
+text_list = ["ant", "bot", "cat", "dog", "egg", "fan", "goat"]
